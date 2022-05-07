53dc5933dccaae4d77dee7a357a5fe8a66fb916b
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-05-07 22:22:44 +0900

    feat: split register form

--- components/app/AuthModal.vue
+++ components/app/AuthModal.vue
@@ -2,6 +2,7 @@
 import BModal from '../base/BModal.vue'
 import BTab from '../base/BTab.vue'
 import Login from './Login.vue'
+import Register from './Register.vue'
 
 const props = defineProps<{ modelValue: boolean }>()
 const emit = defineEmits(['update:modelValue'])
@@ -17,73 +18,10 @@ const modelValue = useVModel(props, 'modelValue', emit)
 
     <BTab :tabs="tabs">
       <template #tab-1>
-        <!-- Login Form -->
         <Login />
       </template>
       <template #tab-2>
-        <!-- Registration Form -->
-        <form>
-          <!-- Name -->
-          <FormKit
-            outer-class="mb-3"
-            label="Name"
-            placeholder="Enter name"
-            name="name"
-            label-class="inline-block mb-2"
-            input-class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition
-              duration-500 focus:outline-none rounded bg-transparent focus:border-indigo-400"
-          />
-          <!-- Email -->
-          <div class="mb-3">
-            <label class="inline-block mb-2">Email</label>
-            <input
-              type="email"
-              class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition duration-500 focus:outline-none rounded bg-transparent focus:border-indigo-400"
-              placeholder="Enter Email"
-            />
-          </div>
-          <!-- Age -->
-          <div class="mb-3">
-            <label class="inline-block mb-2">Age</label>
-            <input
-              type="number"
-              class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition duration-500 focus:outline-none rounded bg-transparent focus:border-indigo-400"
-            />
-          </div>
-          <!-- Password -->
-          <div class="mb-3">
-            <label class="inline-block mb-2">Password</label>
-            <input
-              type="password"
-              class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition duration-500 focus:outline-none rounded bg-transparent focus:border-indigo-400"
-              placeholder="Password"
-            />
-          </div>
-          <!-- Confirm Password -->
-          <div class="mb-3">
-            <label class="inline-block mb-2">Confirm Password</label>
-            <input
-              type="password"
-              class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition duration-500 focus:outline-none rounded bg-transparent focus:border-indigo-400"
-              placeholder="Confirm Password"
-            />
-          </div>
-          <!-- Phone Number -->
-          <div class="mb-3">
-            <label class="inline-block mb-2">Phone Number</label>
-            <input
-              type="text"
-              class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition duration-500 focus:outline-none rounded bg-transparent focus:border-indigo-400"
-              placeholder="Enter Phone Number"
-            />
-          </div>
-          <button
-            type="submit"
-            class="block w-full bg-indigo-400 text-white py-1.5 px-3 rounded transition hover:bg-indigo-500 disabled:opacity-50"
-          >
-            Submit
-          </button>
-        </form>
+        <Register />
       </template>
     </BTab>
   </BModal>
---
+++ components/app/Register.vue
@@ -0,0 +1,89 @@
+<script lang="ts" setup>
+const castNumber = (node) => {
+  node.hook.input((value, next) => next(Number(value)))
+}
+</script>
+
+<template>
+  <!-- <app-alert *ngIf="showAlert" [color]="alertColor">
+  {{ alertMsg }}
+</app-alert> -->
+  <form @submit.prevent>
+    <!-- Name -->
+    <FormKit
+      outer-class="mb-3"
+      label="Name"
+      placeholder="Enter Name"
+      name="name"
+      label-class="inline-block mb-2"
+      input-class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition
+              duration-500 focus:(outline-none border-indigo-400) rounded bg-transparent"
+      validation="required|min:3"
+    />
+    <!-- Email -->
+    <FormKit
+      outer-class="mb-3"
+      label="Email"
+      placeholder="Enter Email"
+      name="email"
+      label-class="inline-block mb-2"
+      input-class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition
+              duration-500 focus:(outline-none border-indigo-400) rounded bg-transparent"
+      validation="required|email"
+    />
+    <!-- Age -->
+    <FormKit
+      type="number"
+      outer-class="mb-3"
+      label="Age"
+      placeholder="Enter Age"
+      name="age"
+      label-class="inline-block mb-2"
+      input-class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition
+              duration-500 focus:(outline-none border-indigo-400) rounded bg-transparent"
+      :plugins="[castNumber]"
+      validation="required|min:18|max:120"
+    />
+    <!-- Password -->
+    <FormKit
+      type="password"
+      outer-class="mb-3"
+      label="Password"
+      placeholder="Enter Password"
+      name="password"
+      label-class="inline-block mb-2"
+      input-class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition
+              duration-500 focus:(outline-none border-indigo-400) rounded bg-transparent"
+      validation="required"
+    />
+    <!-- Confirm Password -->
+    <FormKit
+      type="password"
+      outer-class="mb-3"
+      label="Confirm Password"
+      placeholder="Confirm Password"
+      name="confirm_password"
+      label-class="inline-block mb-2"
+      input-class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition
+              duration-500 focus:(outline-none border-indigo-400) rounded bg-transparent"
+      validation="required"
+    />
+    <!-- Phone Number -->
+    <FormKit
+      outer-class="mb-3"
+      label="Phone Number"
+      placeholder="Enter Phone Number"
+      name="phone_number"
+      label-class="inline-block mb-2"
+      input-class="block w-full py-1.5 px-3 text-gray-200 border border-gray-400 transition
+              duration-500 focus:(outline-none border-indigo-400) rounded bg-transparent"
+      validation="required"
+    />
+    <button
+      type="submit"
+      class="block w-full bg-indigo-400 text-white py-1.5 px-3 rounded transition hover:bg-indigo-500 disabled:(opacity-50 bg-indigo-400)"
+    >
+      Submit
+    </button>
+  </form>
+</template>
