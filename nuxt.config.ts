50d0f80e49d48cd036d276328b4d5cb4d51ad03e
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-05-07 13:42:04 +0900

    chore: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,14 @@ import { transformerDirectives, transformerVariantGroup } from 'unocss'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
-    '@unocss/nuxt'
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
   ],
   unocss: {
     attributify: true,
     transformers: [
       transformerDirectives(),
-      transformerVariantGroup()
-    ]
-  }
+      transformerVariantGroup(),
+    ],
+  },
 })
